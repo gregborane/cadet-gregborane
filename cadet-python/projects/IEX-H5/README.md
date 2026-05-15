299bf16f18940e41823564ac432dc656180c042a
gregborane <madaradellac@gmail.com>
2026-05-15 18:01:23 -0400

    finished the process

---
+++ cadet-python/projects/IEX-H5/README.md
@@ -0,0 +1,19 @@
+# Proteins in IEX chr
+
+H5 + 2 proteins
+
+# Schematic of the project 
+
+5 components : 3 proteins + buffer + salt
+
+Inlet1 = proteins
+Inlet2 = buffer + salt
+
+```
+-sec_003------ Inlet1 ---|
+                         |
+-sec_000------ Inlet2 ----->--[ Dumper ]-sec_004--[ Col ]-sec_005--[ Outlet ]
+                         |
+-sec_001------ Inlet3 ---|
+
+```
--- cadet-python/projects/IEX-H5/multilang-col-multi.py
+++ cadet-python/projects/IEX-H5/multilang-col-multi.py
@@ -1,139 +1,313 @@
-#!/bin/bash
-
-import os, subprocess
+import sys, os, subprocess
 from cadet import Cadet
 import numpy as np
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 
+sys.path.append("..")
 load_dotenv()
 
 # Cadet.cadet_path = os.getenv("cadet_root_dir")
 
-
+# %%
 # Define system settings
 
 ## General
-init_c = [1.0, 0]
-ncomp = len(init_c)  # number of components to consider in the chromatographic medium
-t_cycle = 30  # in s, total time for the global unit to operate
-Q = 6e-6  # in m**-3/s, flow rate
-V_mixer = 0.5  # m**-3, Volume of the reactor
-## Reaction
-kfwd = [0.1]  # in SI
-kbwd = [0.0]  # in SI
-stoich = [-1, 1]  # -1 -> +1
+column_length = 0.025  ## m
+column_volume = 5.025e-6  ## m^3
+protein_MW = 150  ## kDa
+column_porosity = 0.31  ## 1
+particle_porosity = 0.95  ## 1
+Q = 1.25 / (6e7)  ## m**-3/s
+elution_pH_start = 5.5
+elution_pH_end = 3.3
+loading = 0.097  ## mg/mL
+c_feed = 4.0 / protein_MW  ## mol/m^3
+RT = column_volume / Q  ## s
+total_porosity = column_porosity + (1.0 - column_porosity) * particle_porosity
+ncomp = 5
+V_mixer = 10000000
+t_cycle = 45 * 60
+# %%
 
 # Initialization
 model = Cadet()
 
-# Inlet profile : https://cadet.github.io/v6.0.0a1/interface/unit_operations/inlet.html#inlet-config
-## Define the inlet
-model.root.input.model.unit000.unit_type = "INLET"
-## Number of components in the unit (sec)
-model.root.input.model.unit000.ncomp = ncomp
-## Mathematic model for the intlet
-model.root.input.mode.unit000.intlet_type = "PIECEISE_CUBIC_POLY"  # Only existing one
-## Concentration variation in each unit in mol/m**-3
-### = a + b (t-t_i) + c(t-t_i**2) + d (t-t_i**3)
-model.root.input.model.unit_000.sec_000.const_coeff = [
+# %% [markdown]
+
+"""
+```
+-sec_001------ Inlet1 ---|
+                         |
+-sec_000------ Inlet2 ----->--[ Dumper ]-sec_003--[ Col ]-sec_004--[ Outlet ]
+
+```
+"""
+
+# %%
+# Create Inlet
+# Analyte injection
+model.root.input.model.unit_000.unit_type = "INLET"
+model.root.input.model.unit_000.ncomp = ncomp
+model.root.input.model.unit_000.inlet_type = "PIECEISE_CUBIC_POLY"  # Only existing one
+model.root.input.model.unit_000.sec_001.const_coeff = [
     1.0,
-]  # a
+    1.0,
+    1.0,
+    0.0,
+    0.0,
+]
+model.root.input.model.unit_000.sec_001.lin_coeff = [
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+]
+model.root.input.model.unit_000.sec_001.quad_coeff = [
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+]
+model.root.input.model.unit_000.sec_001.cube_coeff = [
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+]
+
+# Buffer injection
+model.root.input.model.unit_001.unit_type = "INLET"
+model.root.input.model.unit_001.ncomp = ncomp
+model.root.input.model.unit_001.intet_type = "PIECEISE_CUBIC_POLY"  # Only existing one
+model.root.input.model.unit_001.sec_000.const_coeff = [
+    0.0,
+    0.0,
+    0.0,
+    1.0,
+    1.0,
+]
 model.root.input.model.unit_000.sec_000.lin_coeff = [
     0.0,
-]  # b
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+]
 model.root.input.model.unit_000.sec_000.quad_coeff = [
     0.0,
-]  # c
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+]
 model.root.input.model.unit_000.sec_000.cube_coeff = [
     0.0,
-]  # d
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+]
 
-## Create the unit
+## Create dumper
 model.root.input.model.unit_001.unit_type = "CSTR"
-## number of componenets in the unit
 model.root.input.model.unit_001.ncomp = ncomp
-## volume f the unit
 model.root.input.model.unit_001.init_volume = V_mixer
-# concentration of the compenents in the unit
 model.root.input.model.unit_001.init_c = [
     0.0,
     0.0,
+    0.0,
+    0.0,
+    0.0,
 ]
-## Concentration variation in each unit in mol/m**-3
-### = a + b (t-t_i) + c(t-t_i**2) + d (t-t_i**3)
 model.root.input.model.unit_001.sec_000.const_coeff = [
-    1.0,
-]  # a
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+]
 model.root.input.model.unit_001.sec_000.lin_coeff = [
     0.0,
-]  # b
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+]
 model.root.input.model.unit_001.sec_000.quad_coeff = [
     0.0,
-]  # c
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+]
 model.root.input.model.unit_001.sec_000.cube_coeff = [
     0.0,
-]  # d
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+]
 
-# Columns : Build like a reactor without the reaction
+# Create the column and its properties
+model.root.inut.model.unit_002.unit_type = "GENERAL_RATE_MODEL"
+model.root.input.model.unit_002.ncomp = ncomp
+model.root.input.model.unit_002.cross_section_area = column_volume / column_length
+model.root.input.model.unit_002.col_length = column_length
+model.root.input.model.unit_002.col_porosity = column_porosity  # 1
+model.root.input.model.unit_002.col_dispersion = 1.36e-8  # m^2/s
+model.root.input.model.unit_002.par_porosity = particle_porosity  # 1
+model.root.input.model.unit_002.par_radius = 0.0425e-3  # m
+model.root.input.model.unit_002.par_diffusion = [
+    1,
+    1,
+    1,
+    1,
+    1,
+]
+model.root.input.model.unit_002.par_surfdiffusion = [
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+]
+model.root.input.model.unit_002.film_diffusion = [
+    1,
+    1,
+    1,
+    1,
+    1,
+]
+model.root.input.model.unit_002.init_c = [
+    9,
+    9,
+    9,
+    1,
+    1,
+]
+model.root.input.model.unit_002.init_q = [
+    9,
+    9,
+    9,
+    1,
+    1,
+]
+# Create resolution grid
+model.root.input.model.unit_001.discretization.ncol = 50
+model.root.input.model.unit_001.discretization.npar = 12
+model.root.input.model.unit_001.discretization.nbound = [
+    1.0,
+    1.0,
+    1.0,
+    1.0,
+    1.0,
+]
+model.root.input.model.unit_001.discretization.par_disc_type = "EQUIDISTANT_PAR"
+model.root.input.model.unit_001.discretization.use_analytic_jacobian = 1
+model.root.input.model.unit_001.discretization.reconstruction = "WENO"
+model.root.input.model.unit_001.discretization.gs_type = 1
+model.root.input.model.unit_001.discretization.max_krylov = 0
+model.root.input.model.unit_001.discretization.max_restarts = 10
+model.root.input.model.unit_001.discretization.schur_safety = 1.0e-8
+model.root.input.model.unit_001.discretization.weno.boundary_model = 0
+model.root.input.model.unit_001.discretization.weno.weno_eps = 1e-10
+model.root.input.model.unit_001.discretization.weno.weno_order = 2
 
+# Create Isotherm
+model.root.input.model.unit_002.adsorption_model = "MULTI_COMPONENT_LANGMUIR"
+model.root.input.model.unit_002.adsorption_model_multiplex = 1
+model.root.input.model.unit_002.is_kinetic = 1
+model.root.input.model.unit_002.mcl_ka = [
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+]
+
+model.root.input.model.unit_002.mcl_kd = [
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+]
 
 # Outlet profile
-## Define the outlet
-model.root.input.model.unit_002.unit_type = "OUTLET"
-## Number of components in the unit
-model.root.input.model.unit_002.ncomp = 1
-## Concentration variation in each unit in mol/m**-3
-### = a + b (t-t_i) + c(t-t_i**2) + d (t-t_i**3)
-model.root.input.model.unit_002.sec_001.const_coeff = [
+model.root.input.model.unit_004.unit_type = "OUTLET"
+model.root.input.model.unit_004.ncomp = ncomp
+model.root.input.model.unit_004.sec_004.const_coeff = [
+    1.0,
+    1.0,
+    1.0,
     0.0,
-]  # a
-model.root.input.model.unit_002.sec_001.lin_coeff = [
     0.0,
-]  # b
-model.root.input.model.unit_002.sec_001.quad_coeff = [
+]
+model.root.input.model.unit_004.sec_004.lin_coeff = [
     0.0,
-]  # c
-model.root.input.model.unit_002.sec_001.cube_coeff = [
     0.0,
-]  # d
-
+    0.0,
+    0.0,
+    0.0,
+]
+model.root.input.model.unit_004.sec_004.quad_coeff = [
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+]
+model.root.input.model.unit_004.sec_004.cube_coeff = [
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+]
 
 # Defining time passage in each section
-model.root.input.solver.sections.nsec = 2  # number of sections : create discontinuit in the process (injection of specific volume/ concentration)
+model.root.input.solver.sections.nsec = 5
 model.root.input.solver.sections.section_times = [
-    0.0,
-    t_cycle / 2,
-    t_cycle,
-]  # intervals between sections
-model.root.input.solver.sections.section_continuity = [
-    0
-]  # Continuity (restart at zero between sections)
+    0.0,  # initialization
+    t_cycle,  # wash
+    t_cycle,  # pH
+    t_cycle,  # elution
+    t_cycle,  # outlet
+]
+
+model.root.input.solver.sections.section_continuity = [0]
 model.root.input.model.solver.user_solution_times = np.linspace(0, t_cycle, 1001)
 
 # Connecting the element
-## number of valve (connector)
 model.root.input.model.connections.nswitches = 1
-##
 model.root.input.model.connections.switch_000.section = 0
-## Matrix of connectivity is defined as : [UnitOpID from, UnitOpID to, Component from, Component to, volumetric flow rate]
 model.root.input.model.connections.switch_000.connections = [
-    (0, 1, -1, -1, Q),  # unit_000, unit_001, all components, all components, Q/ m^3/s
-    (1, 2, -1, -1, Q),
+    (0, 2, -1, -1, Q),  # inlet one into dumper
+    (1, 2, -1, -1, Q),  # inlet two into dumper
+    (2, 3, -1, -1, Q),  # dumper into col
+    (3, 4, -1, -1, Q),  # col into outlet
 ]
 
-# run a simulation
-## save results
-model.filename = "filename.h5"
-## save parameters
+# Run a simulation
+model.filename = f"{os.getenv('h5_iex_path')}/simulation_of_.h5"
 model.save()
-## run the simulation
 data = model.run()
-## load results
 model.load()
-
-# get the results
-##
 time = model.root.output.solution.solution_times
-##
 c = model.root.output.solution.unit_001.solution_outlet_comp_001
+
+# %%
+# Plot results
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.plot(time[1:] / 60 * Q * 6e7, c[1:] * protein_MW, c="orange", label="mAb")
+ax.set_xlabel(r"Volume/mL")
+ax.set_ylabel(r"Concentration/(mg/mL)")
+ax_ph = ax.twinx()
+ax_ph.set_ylabel("pH")
+plt.show()
+plt.savefig(f"{os.getenv('graph_iex_path')}/results.png")
